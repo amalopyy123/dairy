2f96b61b57fda2ff4a27e91c957fef44d03b4f46
HC <hclee@bcnetcom.com>
2024-01-26 21:42:06 +0800

    add project files

---
+++ dairy1.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dairy
+-- ------------------------------------------------------
+-- Server version	5.7.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `note`
+--
+
+DROP TABLE IF EXISTS `note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `note` (
+  `nId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `day` int(10) unsigned DEFAULT NULL,
+  `content` text,
+  `memo` text,
+  `updated_at` date DEFAULT NULL,
+  PRIMARY KEY (`nId`) USING BTREE,
+  UNIQUE KEY `note_day_index` (`day`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note`
+--
+
+LOCK TABLES `note` WRITE;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+INSERT INTO `note` VALUES (3,1,'123123','5','2024-01-25'),(4,2,'213123','213123','2024-01-26');
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-26 20:33:54
---
+++ function.php
@@ -0,0 +1,221 @@
+<?php
+date_default_timezone_set('PRC');
+
+
+class DbOperation
+{
+    protected $db_user = 'root';
+    protected $db_password = '';
+    protected $db_host = '127.0.0.1';
+    protected $db_port = 3306;
+    protected $db_name = 'dairy';
+    protected $db_table = 'note';
+
+    public function selectByFields($fields, $value)
+    {
+        if (empty($fields)) {
+            return false;
+        }
+        //connect
+        $link = new mysqli($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        if ($link->connect_errno) {
+            throw new Exception("Connect failed: {$link->connect_error}");
+        }
+        $ret = array();
+        $whereArray = [];
+        foreach ($fields as $field) {
+            if (in_array($field, ["nId", "day", "content", "memo"])) {
+                $whereArray[] = "`" . $field . "` like '%" . $value . "%'";
+            }
+        }
+        $whereClause = implode(' or ', $whereArray);
+        $sql = <<<SQL
+select * from `note` where {$whereClause}
+SQL;
+        $result = $link->query($sql);
+        if ($result) {
+            // Cycle through results
+            while ($row = $result->fetch_object()) {
+                array_push($ret, (array)$row);
+            }
+            // Free result set
+            $result->close();
+        }
+        //close
+        $link->close();
+        return $ret;
+    }
+
+    public function searchByDay($fromDay, $toDay)
+    {
+        //connect
+        $link = new mysqli($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        if ($link->connect_errno) {
+            throw new Exception("Connect failed: {$link->connect_error}");
+        }
+        $ret = array();
+        $whereArray = [];
+        if ($fromDay) {
+            $whereArray[] = "day >=" . $fromDay;
+        }
+        if ($toDay) {
+            $whereArray[] = "day <=" . $toDay;
+        }
+        $whereClause = implode(' and ', $whereArray);
+        $sql = <<<SQL
+select * from `note` where {$whereClause}
+SQL;
+        $result = $link->query($sql);
+        if ($result) {
+            // Cycle through results
+            while ($row = $result->fetch_object()) {
+                array_push($ret, (array)$row);
+            }
+            // Free result set
+            $result->close();
+        }
+        //close
+        $link->close();
+        return $ret;
+    }
+
+    public function updateFields($nId, $day, $content, $memo, $today)
+    {
+        //connect
+        $link = new mysqli($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        if ($link->connect_errno) {
+            throw new Exception("Connect failed: {$link->connect_error}");
+        }
+        $ret = array();
+
+        $sql = <<<SQL
+update `note` set `day`={$day},`content`='{$content}',`memo`='{$memo}',`updated_at`='{$today}' where `nid`={$nId}
+SQL;
+        $result = $link->query($sql);
+
+        //close
+        $link->close();
+        return $result;
+    }
+
+    public function newRecord($day, $content, $memo, $today)
+    {
+        //connect
+        $link = new mysqli($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        if ($link->connect_errno) {
+            throw new Exception("Connect failed: {$link->connect_error}");
+        }
+        $ret = array();
+
+        $sql = <<<SQL
+insert into `note`(`day`,`content`,`memo`,`updated_at`) values ({$day},'{$content}','{$memo}','{$today}');
+SQL;
+        $result = $link->query($sql);
+        if ($result) {
+            //必需在close之前获取
+            $linkId = $link->insert_id;
+            //close
+            $link->close();
+            return $linkId;
+        }
+        //close
+        $link->close();
+        return $result;
+    }
+
+    public function getDays()
+    {
+        //connect
+        $link = new mysqli($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        if ($link->connect_errno) {
+            throw new Exception("Connect failed: {$link->connect_error}");
+        }
+        $ret = array();
+        $sql = <<<SQL
+select day from `note`
+SQL;
+        $result = $link->query($sql);
+        if ($result) {
+            // Cycle through results
+            while ($row = $result->fetch_object()) {
+                array_push($ret, (array)$row);
+            }
+            // Free result set
+            $result->close();
+        }
+        //close
+        $link->close();
+        return $ret;
+    }
+}
+
+
+function handleRequest()
+{
+    $postData = $_POST;
+    $retJson = ['status' => false, "message" => 'aaa', 'data' => []];
+    $dbOpration = new DbOperation();
+    $today = date('Y-m-d');
+    switch ($_POST['method']) {
+        case 'search':
+        {
+            $queryData = $dbOpration->selectByFields(explode(',', $_POST['fields']), $_POST['value']);
+            $retJson['status'] = true;
+            $retJson['data'] = $queryData;
+            $retJson['message'] = 'success';
+            return json_encode($retJson);;
+        }
+        case 'searchByDay':
+        {
+            $queryData = $dbOpration->searchByDay($_POST['from_day'], $_POST['to_day']);
+            $retJson['status'] = true;
+            $retJson['data'] = $queryData;
+            $retJson['message'] = 'success';
+            return json_encode($retJson);;
+        }
+        case 'update':
+        {
+            $result = $dbOpration->updateFields($_POST['nId'], $_POST['day'], $_POST['content'], $_POST['memo'], $today);
+            if ($result) {
+                $retJson['status'] = true;
+                $retJson['updated_at'] = $today;
+                $retJson['message'] = 'Success to update';
+                $retJson['days'] = $dbOpration->getDays();
+            } else {
+                $retJson['status'] = false;
+                $retJson['message'] = 'Failed to update';
+            }
+            return json_encode($retJson);;
+        }
+        case 'insert':
+        {
+            $result = $dbOpration->newRecord($_POST['day'], $_POST['content'], $_POST['memo'], $today);
+            if ($result) {
+                $retJson['status'] = true;
+                $retJson['updated_at'] = $today;
+                $retJson['message'] = 'Success to insert';
+                $retJson['nId'] = $result;
+                $retJson['days'] = $dbOpration->getDays();
+            } else {
+                $retJson['status'] = false;
+                $retJson['message'] = 'Failed to insert';
+            }
+            return json_encode($retJson);;
+        }
+        case 'getDays':
+        {
+            $queryData = $dbOpration->getDays();
+            $retJson['status'] = true;
+            $retJson['data'] = $queryData;
+            $retJson['message'] = 'success';
+            return json_encode($retJson);;
+        }
+        default:
+        {
+            return json_encode($retJson);;
+        }
+    }
+}
+
+$retJson = handleRequest();
+echo $retJson;
